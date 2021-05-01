2b2d1159de5b4eadd8ffc2f6de81292687bca358
admiralpxl <wnstnjr@gmail.com>
2021-05-01 19:52:15 -0400

    Arreglos 2

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const tl = gsap.timeline({defaults:{duration: 1}});
 tl.from(".logo", {y: 30, opacity: 0})
 .from(".ani1", {y: -10, stagger: .3, opacity: 0})
 .from(".anim1", {y: -50, stagger: .6, opacity: 0})
+.from(".an2", {x: -40, stragger: 1, opacity: 0}, "-=2")
 .from(".anim2",{y: 50, opacity: 0}, "-=1.5")
 .from('.anidiv', {x: -30, opacity: 0}, "-=.5")
 .from(".anims2", {x: 30, opacity: 0, ease: "elastic(1, 03)"}, "-=.5")
