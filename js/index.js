59d129a5a43c32fbc7553b54c79f51561dcf4f12
admiralpxl <wnstnjr@gmail.com>
2021-07-04 21:56:38 -0400

    archivos agregados

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const tl = gsap.timeline({defaults:{duration: 1}});
-//Aqui el orden importa
-tl.from(".logo", {y: 30, opacity: 0})
-.from(".ani1", {y: -10, stagger: .3, opacity: 0})
-.from(".anim1", {y: -50, stagger: .6, opacity: 0})
-.from(".anim2",{y: 50, opacity: 0}, "-=1.5")
-.from('.anidiv', {x: 50, opacity: 0}, "-=.5")
