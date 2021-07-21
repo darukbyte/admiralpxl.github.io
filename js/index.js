13e346ed976869f74f7db95f9c94c94841cf5181
admiralpxl <wnstnjr@gmail.com>
2021-07-21 17:49:04 -0400

    animaciones agregadas

--- js/index.js
+++ js/index.js
@@ -2,10 +2,31 @@ const open = document.getElementById('open');
 const menu = document.getElementById('menu');
 const button = document.getElementById('button');
 const c = document.getElementById('c');
+//Animation on Scroll - code below.
+let itemAppear = [...document.querySelectorAll('.animation')];
+let options = {
+    rootMargin: '-10%',
+    threshold: 0.0
+};
+let observer = new IntersectionObserver(showItem, options);
+
+function showItem (entries) {
+    console.log(entries);
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('active');
+        }
+    })
+}
+
+itemAppear.forEach(item => {
+    observer.observe(item);
+});
 
 
 const cerrar = () => {
     open.classList.remove('show');
+    open.classList.add('unshow');
     c.classList.remove('button-div-open');
 }
 
@@ -13,8 +34,12 @@ function menuOpenButton() {
     if(c.classList.contains('button-div-open')) {
         c.classList.remove('button-div-open');
         open.classList.remove('show');
+        open.classList.add('unshow');
     } else {
         c.classList.add('button-div-open');
         open.classList.add('show');
+        open.classList.remove('unshow');
     }
 }
+
+
